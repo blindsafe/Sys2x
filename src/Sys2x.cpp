a9a0071cbc01dd0819b5784fe2e3445958a7aa5f
charliey1001 <charliey1001@gmail.com>
2021-08-31 16:01:24 -0700

    Initial test

---
+++ src/Sys2x.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Sys2x.cpp
+// Author      : Charliey1001
+// Version     :
+// Copyright   : my copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "sys2 going to try more" << endl; // prints sys2 going to try more
+	return 0;
+}
